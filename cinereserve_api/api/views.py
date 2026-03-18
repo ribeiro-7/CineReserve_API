afa11361da9e1b9533f70ef0105f82e0bf92a27f
ribeirin-7 <arthurr366@gmail.com>
2026-03-18 15:28:07 -0300

    Criando as primeiras views para teste

--- cinereserve_api/api/views.py
+++ cinereserve_api/api/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Movie, Session
+from .serializers import MovieSerializer, SessionSerializer
 
-# Create your views here.
+
+@api_view()
+def movie_list(request):
+    movies = Movie.objects.all().prefetch_related('sessions')
+    serializer = MovieSerializer(movies, many=True)
+    return Response(serializer.data)
+
+@api_view()
+def session_list(request):
+    session = Session.objects.all()
+    serializer = SessionSerializer(session, many=True)
+    return Response(serializer.data)
