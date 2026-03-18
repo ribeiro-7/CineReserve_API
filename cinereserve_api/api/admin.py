9e33267a0afbfe13a118b0a54c148da5b70af71a
ribeirin-7 <arthurr366@gmail.com>
2026-03-18 15:26:32 -0300

    Registrando os models na admin para criar objetos no banco de dados e testar

--- cinereserve_api/api/admin.py
+++ cinereserve_api/api/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Movie, Session
+
+# Configuração para Movie
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ('title', 'genre', 'age_rating', 'duration', 'release_date')
+    list_filter = ('genre', 'age_rating')
+    search_fields = ('title', 'description')
+
+# Configuração para Session
+@admin.register(Session)
+class SessionAdmin(admin.ModelAdmin):
+    list_display = ('movie', 'date', 'showtime', 'theater')
+    list_filter = ('date', 'theater')
+    search_fields = ('movie__title', 'theater')
