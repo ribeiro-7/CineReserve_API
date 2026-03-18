b24d694d21e22fa3a45e0e1581ab87e41504af82
ribeirin-7 <arthurr366@gmail.com>
2026-03-18 15:27:37 -0300

    Criando os models 'movie', 'session' e utilizando o model do proprio django auth

--- cinereserve_api/api/models.py
+++ cinereserve_api/api/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Movie(models.Model):
+
+    AGE_RATINGS = [
+        ('L', "Livre"),
+        ('10', "Não recomendado para menores de 10 anos"),
+        ('12', "Não recomendado para menores de 12 anos"),
+        ('14', "Não recomendado para menores de 14 anos"),
+        ('16', "Não recomendado para menores de 16 anos"),
+        ('18', "Não recomendado para menores de 18 anos")
+    ]
+
+    GENRES = [
+        ("Ação", "Ação"),
+        ("Aventura", "Aventura"),
+        ("Comédia", "Comédia"),
+        ("Drama", "Drama"),
+        ("Ficção Científica", "Ficção Científica"),
+        ("Terror ", "Terror"),
+        ("Suspense ", "Suspense"),
+        ("Romance", "Romance"),
+        ("Fantasia", "Fantasia"),
+        ("Musical", "Musical"),
+        ("Documentário", "Documentário"),
+    ]
+
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    duration = models.PositiveIntegerField() #duração em minutos - ex: 120min
+    age_rating = models.CharField(max_length=2,choices=AGE_RATINGS) 
+    genre = models.CharField(max_length=50, choices=GENRES)
+    release_date = models.DateField()
+
+class Session(models.Model):
+    date = models.DateField()
+    showtime = models.TimeField()
+    theater = models.CharField(max_length=50)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE, related_name="sessions")
