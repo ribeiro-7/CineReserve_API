10c434bd08c772badbb3d8973b1e8d253fe98163
ribeirin-7 <arthurr366@gmail.com>
2026-03-18 22:43:55 -0300

    Implementando ViewSet para os mostrar os filmes, filme especifico com sua sessões disponíveis... Seu CRUD completo

--- cinereserve_api/api/views.py
+++ cinereserve_api/api/views.py
@@ -1,18 +1,12 @@
-from django.shortcuts import render
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from .models import Movie, Session
-from .serializers import MovieSerializer, SessionSerializer
+from .models import Movie
+from .serializers import MovieListSerializer, MovieDetailSerializer
+from rest_framework.viewsets import ModelViewSet
 
 
-@api_view()
-def movie_list(request):
-    movies = Movie.objects.all().prefetch_related('sessions')
-    serializer = MovieSerializer(movies, many=True)
-    return Response(serializer.data)
+class MovieViewSet(ModelViewSet):
+    queryset = Movie.objects.all().prefetch_related('sessions')
 
-@api_view()
-def session_list(request):
-    session = Session.objects.all()
-    serializer = SessionSerializer(session, many=True)
-    return Response(serializer.data)
+    def get_serializer_class(self):
+        if self.action == 'retrieve':
+            return MovieDetailSerializer
+        return MovieListSerializer
