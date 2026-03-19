5a9de8e49ca17d57bbf04e698acccc99630b0c20
ribeirin-7 <arthurr366@gmail.com>
2026-03-19 13:17:23 -0300

    Implementando View para Registrar usuário

--- cinereserve_api/api/views.py
+++ cinereserve_api/api/views.py
@@ -1,6 +1,9 @@
 from .models import Movie
-from .serializers import MovieListSerializer, MovieDetailSerializer
+from .serializers import MovieListSerializer, MovieDetailSerializer, RegisterUserSerializer
+from rest_framework import status
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 
 class MovieViewSet(ModelViewSet):
@@ -10,3 +13,13 @@ class MovieViewSet(ModelViewSet):
         if self.action == 'retrieve':
             return MovieDetailSerializer
         return MovieListSerializer
+
+
+class RegisterUserView(APIView):
+    
+    def post(self, request):
+        serializer = RegisterUserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
