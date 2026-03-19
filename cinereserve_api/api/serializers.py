e83f6b305b9424476046b42b1665621f6ed90c50
ribeirin-7 <arthurr366@gmail.com>
2026-03-18 22:42:34 -0300

    Implementando os ModelSerializers

--- cinereserve_api/api/serializers.py
+++ cinereserve_api/api/serializers.py
@@ -1,15 +1,18 @@
 from rest_framework import serializers
 from .models import Movie, Session
 
+class MovieListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Movie
+        fields = ['title', 'description', 'duration', 'age_rating', 'genre', 'release_date']
 
 class SessionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Session
         fields = ['date', 'showtime', 'theater']
 
-class MovieSerializer(serializers.ModelSerializer):
+class MovieDetailSerializer(serializers.ModelSerializer):
     sessions = SessionSerializer(many=True, read_only=True)
-
     class Meta:
         model = Movie
         fields = ['title', 'description', 'duration', 'age_rating', 'genre', 'release_date', 'sessions']
