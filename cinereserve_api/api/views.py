4a00a9851d7e4b2bc123c95bd4da2fe58dfa04d5
ribeirin-7 <arthurr366@gmail.com>
2026-03-19 16:53:03 -0300

    Protegendo requisições com autenticação JWT e adicionando ViewSet para Session também

--- cinereserve_api/api/views.py
+++ cinereserve_api/api/views.py
@@ -1,18 +1,40 @@
-from .models import Movie
-from .serializers import MovieListSerializer, MovieDetailSerializer, RegisterUserSerializer
+from .models import Movie, Session
+from .serializers import MovieListSerializer, MovieDetailWithSessionSerializer, SessionSerializer, RegisterUserSerializer, SessionDetailSerializer
 from rest_framework import status
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.permissions import IsAdminUser, AllowAny
 
 
 class MovieViewSet(ModelViewSet):
     queryset = Movie.objects.all().prefetch_related('sessions')
 
+    #Todos os filmes/filme específico com sessões disponiveis
     def get_serializer_class(self):
         if self.action == 'retrieve':
-            return MovieDetailSerializer
+            return MovieDetailWithSessionSerializer
         return MovieListSerializer
+    
+    #Os outros metodos somente Admin pode alterar, criar, ou deletar filmes
+    def get_permissions(self):
+        if self.action in ['create', 'partial_update', 'update', 'destroy']:
+            return [IsAdminUser()]
+        return [AllowAny()]
+    
+class SessionViewSet(ModelViewSet):
+    queryset = Session.objects.all().prefetch_related('movie')
+
+    def get_serializer_class(self):
+        if self.action == 'retrieve':
+            return SessionDetailSerializer
+        return SessionSerializer
+
+    def get_permissions(self):
+        if self.action in ['create', 'partial_update', 'update', 'destroy']:
+            return [IsAdminUser()]
+        return [AllowAny()]
+
 
 
 class RegisterUserView(APIView):
