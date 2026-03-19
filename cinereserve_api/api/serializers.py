517b4304cb77b2acf758005fffbe97e982f0a9f2
ribeirin-7 <arthurr366@gmail.com>
2026-03-19 13:28:32 -0300

    Implementando Serializer para Registro de User com validação

--- cinereserve_api/api/serializers.py
+++ cinereserve_api/api/serializers.py
@@ -1,5 +1,8 @@
 from rest_framework import serializers
 from .models import Movie, Session
+from django.contrib.auth.models import User
+from django.contrib.auth.password_validation import validate_password
+from .validators import password_validator
 
 class MovieListSerializer(serializers.ModelSerializer):
     class Meta:
@@ -16,3 +19,22 @@ class MovieDetailSerializer(serializers.ModelSerializer):
     class Meta:
         model = Movie
         fields = ['title', 'description', 'duration', 'age_rating', 'genre', 'release_date', 'sessions']
+
+class RegisterUserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = ['email', 'username', 'password']
+        #a senha não é retornada
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
+
+    def validate_password(self, value):
+        validate_password(value)
+        password_validator(value)
+        return value
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
