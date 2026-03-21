ba203b7cdff98b8c575a64de183e434137f9fd0d
ribeirin-7 <arthurr366@gmail.com>
2026-03-21 17:54:17 -0300

    Implementando endpoints para ver os assentos disponiveis em uma sessão específica, reserva de um assento de uma sessão específica com limite de 10 minutos para expirar e compra de um assento de uma sessão específica

--- cinereserve_api/api/views.py
+++ cinereserve_api/api/views.py
@@ -1,11 +1,18 @@
 from .models import Movie, Session
-from .serializers import MovieListSerializer, MovieDetailWithSessionSerializer, SessionSerializer, RegisterUserSerializer, SessionDetailSerializer
+from .serializers.movieserializers import MovieListSerializer, MovieDetailWithSessionSerializer
+from .serializers.sessionserializers import SessionSerializer, SessionDetailSerializer, SeatSessionSerializer
+from .serializers.registerserializer import RegisterUserSerializer
 from rest_framework import status
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAdminUser, AllowAny
 from rest_framework.pagination import PageNumberPagination
+from rest_framework.decorators import action
+from django.shortcuts import get_object_or_404
+from django.utils import timezone
+from datetime import timedelta
+
 
 
 class MoviePagination(PageNumberPagination):
@@ -19,21 +26,75 @@ class MovieViewSet(ModelViewSet):
     queryset = Movie.objects.all().prefetch_related('sessions')
     pagination_class = MoviePagination
 
-    #Todos os filmes/filme específico com sessões disponiveis
     def get_serializer_class(self):
         if self.action == 'retrieve':
             return MovieDetailWithSessionSerializer
         return MovieListSerializer
     
-    #Os outros metodos somente Admin pode alterar, criar, ou deletar filmes
     def get_permissions(self):
         if self.action in ['create', 'partial_update', 'update', 'destroy']:
             return [IsAdminUser()]
         return [AllowAny()]
     
 class SessionViewSet(ModelViewSet):
-    queryset = Session.objects.all().prefetch_related('movie')
+    queryset = Session.objects.all().order_by('date', 'showtime').prefetch_related('movie')
     pagination_class = SessionPagination
+
+    @action(detail=True, methods=['get'])
+    def seats(self, request, pk=None):
+        session = self.get_object()
+        seats = session.seats.all()
+        serializer = SeatSessionSerializer(seats, many=True)
+        return Response(serializer.data)
+    
+    @action(detail=True, methods=['post'])
+    def reserve(self, request, pk=None):
+        session = self.get_object()
+        seat_id = request.data.get('seat_id')
+
+        if not seat_id:
+            return Response({'error': 'seat_id invalid'}, status=status.HTTP_400_BAD_REQUEST)
+
+        seat_session = get_object_or_404(session.seats, id=seat_id)
+
+        if seat_session.status == 'Reserved' and seat_session.reserved_until:
+            if seat_session.reserved_until < timezone.now():
+                seat_session.status = 'Available'
+                seat_session.reserved_until = None
+                seat_session.save()
+
+        if seat_session.status == 'Available':
+            seat_session.status = 'Reserved'
+            seat_session.reserved_until = timezone.now() + timedelta(minutes=10)
+            seat_session.save()
+            return Response({'message': 'Seat reserved for 10 minutes'}, status=status.HTTP_200_OK)
+        
+        return Response({'error': f'This seat is {seat_session.status}'}, status=status.HTTP_400_BAD_REQUEST)
+
+    @action(detail=True, methods=['post'])
+    def buy(self, request, pk=None):
+        session = self.get_object()
+        seat_id = request.data.get('seat_id')
+
+        if not seat_id:
+            return Response({'error': 'seat_id invalid'}, status=status.HTTP_400_BAD_REQUEST)
+        
+        seat_session = get_object_or_404(session.seats, id=seat_id)
+
+        if seat_session.status == 'Reserved':
+            if seat_session.reserved_until and seat_session.reserved_until < timezone.now():
+                seat_session.status = 'Available'
+                seat_session.reserved_until = None
+                seat_session.save()
+
+        if seat_session.status == 'Available':
+            seat_session.status = 'Sold'
+            seat_session.reserved_until = None
+            seat_session.save()
+            return Response({'message': f'You bought a ticket for: "{seat_session.session.movie.title}", your seat is: "{seat_session.seat.row}{seat_session.seat.number}"'})
+        
+        return Response({'error': f'This seat is already {seat_session.status}'})
+
 
     def get_serializer_class(self):
         if self.action == 'retrieve':
@@ -44,7 +105,6 @@ class SessionViewSet(ModelViewSet):
         if self.action in ['create', 'partial_update', 'update', 'destroy']:
             return [IsAdminUser()]
         return [AllowAny()]
-
 
 
 class RegisterUserView(APIView):
