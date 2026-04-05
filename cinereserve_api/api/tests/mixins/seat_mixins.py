b5d968b323b73d1b2edb4b14b2a02de2f94272b2
ribeirin-7 <arthurr366@gmail.com>
2026-04-05 16:58:31 -0300

    Implementando helpers na mixin de seat

---
+++ cinereserve_api/api/tests/mixins/seat_mixins.py
@@ -0,0 +1,54 @@
+from api.models import Seat, SeatSession
+from .session_mixins import SessionMixin
+from django.urls import reverse
+
+class SeatMixin(SessionMixin):
+    def create_seats(self, rows=5, number=10):
+        session = self.create_session()
+        seat_sessions = []
+        for row in ['A', 'B', 'C', 'D', 'E']:
+            for number in range(1, 11):
+                seat, _ = Seat.objects.get_or_create(
+                    row=row,
+                    number=number
+                )
+                seat_session = SeatSession.objects.create(
+                    session=session,
+                    seat=seat,
+                    status='Available'
+                )
+                seat_sessions.append(seat_session)
+        return {
+            'session': session,
+            'seat_sessions': seat_sessions
+        }
+    
+    def create_available_seat(self, row='A', number='1'):
+        session = self.create_session()
+        seat = Seat.objects.create(
+            row=row,
+            number=number
+        )
+        seat_session = SeatSession.objects.create(
+            session=session,
+            seat=seat,
+            status='Available'
+        )
+        
+        return seat_session
+
+    def reserve_seat(self, session_id, seat_id, access_token):
+        api_url = reverse('sessions-reserve', args=[session_id])
+        data = {
+            'seat_id': seat_id
+        }
+        response = self.client.post(api_url, data=data, HTTP_AUTHORIZATION=f'Bearer {access_token}')
+        return response
+    
+    def buy_seat(self, session_id, seat_id, access_token=None):
+        api_url = reverse('sessions-buy', args=[session_id])
+        data = {
+            'seat_id': seat_id
+        }
+        response = self.client.post(api_url, data=data, HTTP_AUTHORIZATION=f'Bearer {access_token}')
+        return response
