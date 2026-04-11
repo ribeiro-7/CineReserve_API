3976705a75ec5bba8dfb4bb5f28901a3cfc560cb
ribeirin-7 <arthurr366@gmail.com>
2026-04-11 14:21:28 -0300

    Implementando throttles para list e retrieve de sessions e movies

--- cinereserve_api/api/throttles.py
+++ cinereserve_api/api/throttles.py
@@ -16,17 +16,51 @@ class SeatsRateThrottle(SimpleRateThrottle):
         if hasattr(self, 'request'):
             request = self.request
             if request.user and request.user.is_authenticated:
-                return '20/min'
-        return '5/min'
+                return '30/min'
+        return '15/min'
+    
+class SessionReadRateThrottle(SimpleRateThrottle):
+    scope = 'session_read'
+
+    def get_cache_key(self, request, view):
+        if request.user and request.user.is_authenticated:
+            return f"{self.scope}_user_{request.user.id}"
+        else:
+            ident = self.get_ident(request)
+            return f"{self.scope}_anon_{ident}"
+
+    def get_rate(self):
+        if hasattr(self, 'request'):
+            request = self.request
+            if request.user and request.user.is_authenticated:
+                return '30/min'
+        return '15/min'
 
 class ReserveRateThrottle(UserRateThrottle):
     scope = 'reserve'
-    rate = '5/min'
+    rate = '10/min'
 
 class BuyRateThrottle(UserRateThrottle):
     scope = 'buy'
-    rate = '5/min'
+    rate = '10/min'
 
 class TicketRateThrottle(UserRateThrottle):
     scope = 'ticket'
-    rate = '30/min'
+    rate = '20/min'
+
+class MovieRateThrottle(SimpleRateThrottle):
+    scope = 'movie'
+
+    def get_cache_key(self, request, view):
+        if request.user and request.user.is_authenticated:
+            return f"{self.scope}_user_{request.user.id}"
+        else:
+            ident = self.get_ident(request)
+            return f"{self.scope}_anon_{ident}"
+
+    def get_rate(self):
+        if hasattr(self, 'request'):
+            request = self.request
+            if request.user and request.user.is_authenticated:
+                return '30/min'
+        return '15/min'
