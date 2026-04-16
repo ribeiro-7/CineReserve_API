05912fd5422e36de5a3cbd4fd7e4895acee7890b
ribeirin-7 <arthurr366@gmail.com>
2026-04-16 12:03:00 -0300

    Adicionando TokenBlackListView para logout

--- cinereserve_api/api/urls.py
+++ cinereserve_api/api/urls.py
@@ -4,7 +4,8 @@ from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
-    TokenVerifyView
+    TokenVerifyView,
+    TokenBlacklistView
 )
 
 router = DefaultRouter()
@@ -16,5 +17,6 @@ urlpatterns = [
     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path('logout/', TokenBlacklistView.as_view(), name='logout'),
     path('', include(router.urls)),
 ]
