9afe7d35af7ba4739edc6802f15df38a07cbf36d
ribeirin-7 <arthurr366@gmail.com>
2026-04-17 11:40:53 -0300

    Adicionando model de Booking para compras de assentos em lote

--- cinereserve_api/booking/models.py
+++ cinereserve_api/booking/models.py
@@ -1,9 +1,24 @@
 from django.db import models
 from django.contrib.auth.models import User
-from cinema.models import SeatSession
+from cinema.models import Session, SeatSession
+
+class Booking(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    session = models.ForeignKey(Session, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(
+        max_length=20,
+        choices=[
+            ('pending', 'Pending'),
+            ('completed', 'Completed'),
+            ('cancelled', 'Cancelled')
+        ],
+        default='pending'
+    )
 
 class Ticket(models.Model):
     code = models.CharField(max_length=100, unique=True)
     purchased_at = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     seat_session = models.OneToOneField(SeatSession, on_delete=models.CASCADE)
+    booking = models.ForeignKey(Booking, on_delete=models.CASCADE, related_name='tickets')
