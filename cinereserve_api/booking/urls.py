a3e226d3c862346ab496a8f613eb2dacbf5ea7f6
ribeirin-7 <arthurr366@gmail.com>
2026-04-17 11:43:37 -0300

    Implementando router para urls de booking

--- cinereserve_api/booking/urls.py
+++ cinereserve_api/booking/urls.py
@@ -1,8 +1,8 @@
-from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from booking import views
 
 router = DefaultRouter()
+router.register(r'bookings', views.BookingViewSet, basename='bookings')
 router.register(r'tickets', views.TicketViewSet, basename='tickets')
 
 urlpatterns = router.urls
