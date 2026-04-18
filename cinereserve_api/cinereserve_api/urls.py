c90a2160b66c6ed45e64c1b6130f38159682a89c
ribeirin-7 <arthurr366@gmail.com>
2026-04-18 17:14:21 -0300

    Adicionando urls do app accounts

--- cinereserve_api/cinereserve_api/urls.py
+++ cinereserve_api/cinereserve_api/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('cinema.urls')),
     path('api/v1/', include('booking.urls')),
+    path('api/v1/', include('accounts.urls')),
 ]
