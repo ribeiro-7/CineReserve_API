dbaa4cfe6bc34c5c02689e3614cd32b8bd669899
ribeirin-7 <arthurr366@gmail.com>
2026-05-16 15:53:18 -0300

    Adicionando documentação de Register

--- cinereserve_api/accounts/views/register_view.py
+++ cinereserve_api/accounts/views/register_view.py
@@ -1,7 +1,13 @@
 from accounts.serializers.registerserializer import RegisterUserSerializer
 from rest_framework.permissions import AllowAny
 from rest_framework.generics import CreateAPIView
+from drf_spectacular.utils import extend_schema
 
+@extend_schema(
+    summary="Register user",
+    description="Creates a new user account.",
+    tags=["Register"],
+)
 class RegisterUserView(CreateAPIView):
     serializer_class = RegisterUserSerializer
     permission_classes = [AllowAny]
