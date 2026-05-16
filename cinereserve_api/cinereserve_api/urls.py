a49da3f2caf80446595048635d4ce4a12ec04b0a
ribeirin-7 <arthurr366@gmail.com>
2026-05-16 15:57:38 -0300

    Adicionando urls do drf-spectacular para documentação dos endpoints da api

--- cinereserve_api/cinereserve_api/urls.py
+++ cinereserve_api/cinereserve_api/urls.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView,
+    SpectacularRedocView,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -7,4 +12,7 @@ urlpatterns = [
     path('api/v1/', include('booking.urls')),
     path('api/v1/auth/', include('accounts.urls')),
     path('payments/', include('payments.urls')),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui',),
+    path('api/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc',),
 ]
