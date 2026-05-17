a41a9affb227de2b63b6bb053a4e3097dde56ae7
ribeirin-7 <arthurr366@gmail.com>
2026-05-17 18:28:58 -0300

    Adicionando configuração de testes no settings.py para usar o arquivo settings_test

--- cinereserve_api/manage.py
+++ cinereserve_api/manage.py
@@ -6,7 +6,13 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cinereserve_api.settings')
+    if 'test' in sys.argv:
+        os.environ.setdefault(
+            'DJANGO_SETTINGS_MODULE',
+            'cinereserve_api.settings_test',
+        )
+    else:
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cinereserve_api.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
